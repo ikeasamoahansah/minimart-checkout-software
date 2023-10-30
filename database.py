45987a08f97fdbc332829b41c2db6ef0bf5c9dff
Ike <dylanraspino264@gmail.com>
2023-10-30 23:16:28 +0000

    excelsheet

--- database.py
+++ database.py
@@ -1,18 +1,18 @@
 import sqlite3
 
-import pandas as pd
+from odf.opendocument import load
 
-FILE_NAME = 'data.xls'
-df = pd.read_excel(FILE_NAME)
+odf_file = 'data.odt'
+doc = load(odf_file)
 
 
 class Database:
 
     def __init__(self):
         self.conn = sqlite3.connect('inventory.db')
+        self.cursor = self.conn.cursor()
         self.create()
         self.populate_data()
-        self.cursor = self.conn.cursor()
         self.conn.commit()
         self.conn.close()
         
@@ -24,11 +24,13 @@ class Database:
         )""")
 
     def populate_data(self):
-        for index, row in df.iterrows():
-            self.cursor.execute("""
-          INSERT INTO Products (product_name, product_price) VALUES (?, ?)
-        """, (row['Product name'], row['Price']))
-        print("data successfully imported")
+      for sheet in doc.spreadsheet.getElementsByType("table:table"):
+          for row in sheet.getElementsByType("table:table-row")[1:]:
+              data = [cell.firstChild.data for cell in row.getElementsByType("table:table-cell")]
+              self.cursor.execute("""
+                INSERT INTO Products (product_name, product_price) VALUES (?,?)
+              """, (data[0], float(data[1])))
+          print("Data imported successfully")
     
 
 db = Database()
