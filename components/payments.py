8e53f615370f34e8e0b8842b7eea7c44ebd5b8ba
Ike <dylanraspino264@gmail.com>
2023-11-06 22:49:52 +0000

    start payments

---
+++ components/payments.py
@@ -0,0 +1,12 @@
+class Payments:
+    def __init__(self) -> None:
+        pass
+
+    def pay(self):
+        pass
+
+
+class MobileMoney(Payments):
+    def __init__(self) -> None:
+        super().__init__()
+        
