5f3775cbab2f8c1b03843fdee54d8a9dff0fc027
Ike <dylanraspino264@gmail.com>
2023-11-06 23:21:24 +0000

    separated from components -> assets

---
+++ assets/payments/momo_pay.py
@@ -0,0 +1,20 @@
+import os
+
+from payments import Payments
+
+from mtnmomo.collection import Collection
+
+client = Collection({
+    "COLLECTION_USER_ID": os.environ.get("COLLECTION_USER_ID"),
+    "COLLECTION_API_SECRET": os.environ.get("COLLECTION_API_SECRET"),
+    "COLLECTION_PRIMARY_KEY": os.environ.get("COLLECTION_PRIMARY_KEY"),
+})
+
+class MobileMoney(Payments):
+    def __init__(self) -> None:
+        super().__init__()
+        
+    def pay(self, number, amount, id):
+        return super().pay()
+    
+    
---
+++ assets/payments/payments.py
@@ -0,0 +1,8 @@
+class Payments:
+    def __init__(self) -> None:
+        pass
+
+    def pay(self):
+        pass
+
+
