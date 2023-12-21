89a899522e7a632c5637a310d6179e6f6dd6ab96
Ike <dylanraspino264@gmail.com>
2023-12-21 22:49:43 +0000

    checkout changes and key bindings

--- api/core/components/checkout.py
+++ api/core/components/checkout.py
@@ -9,25 +9,28 @@ from .payments.momo_pay import MobileMoney
 class Checkout:
     def __init__(self, master):
         self.master = master
+        self.pricemsg = tk.StringVar()
         self.checkout(master)
 
     def checkout(self, master):
         checkout_window = CTkToplevel(master)
         checkout_window.title("Checkout Window")
         checkout_window.geometry("450x300")
-
+        
         checkout_list = tk.Listbox(
             checkout_window, width=40, height=10, selectmode=tk.MULTIPLE
         )
-        checkout_list.pack()
+        checkout_list.pack(pady=5)
 
-        total_price_label = CTkLabel(checkout_window, text="Total Price: ")
+        total_price_label = CTkLabel(checkout_window, textvariable=self.pricemsg)
         total_price_label.pack(padx=5, pady=5)
+
+        checkout_list.bind("<<ListboxSelect>>", self.calculate_total(checkout_list))
 
         calculate_button = CTkButton(
             checkout_window,
-            text="Calculate Total",
-            command=lambda: self.checkout_processing(checkout_list, total_price_label),
+            text="Pay",
+            command=lambda: self.checkout_processing(checkout_list),
         )
         calculate_button.pack(padx=20, pady=20)
 
@@ -40,7 +43,16 @@ class Checkout:
         for product in products:
             checkout_list.insert(tk.END, f"{product[0]} - ${product[1]}")
 
-    def calculate_total(self, checkout_list, total_price_label):
+    def calculate_total(self, checkout_list):
+        total_price = 0
+        idx = checkout_list.curselection()
+        for index in idx:
+            item = checkout_list.get(index)
+            price = float(item.split("$")[-1])
+            total_price += price
+        self.pricemsg.set(f"{total_price}")
+
+    def get_total(self, checkout_list):
         total_price = 0
         selected_indices = checkout_list.curselection()
 
@@ -48,12 +60,11 @@ class Checkout:
             item = checkout_list.get(index)
             price = float(item.split("$")[-1])
             total_price += price
-        total_price_label.configure(text=f"Total Price: ${total_price:.2f}")
 
         return total_price
 
-    def checkout_processing(self, checkout_list, total_price_label):
-        total_price = self.calculate_total(checkout_list, total_price_label)
+    def checkout_processing(self, checkout_list):
+        total_price = self.get_total(checkout_list)
 
         self.payment_processing(total_price)
 
